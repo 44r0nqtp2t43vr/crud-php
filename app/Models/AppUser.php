f2f7cd4564d3405f1687ba3994f6b2eaa02a34ec
jappsa09 <jappsa09@scsServer.local>
2023-03-10 16:42:54 +0800

    added crud

---
+++ app/Models/AppUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppUser extends Model
+{
+    // use HasFactory;
+    protected $table = 'appusers';
+    public $timestamps = false;
+
+    protected $guarded = ['id'];
+    protected $filltable = ['username', 'password', 'name', 'email', 'phone'];
+
+    public static $rules = [
+        'username' => 'required|min:5',
+        'name' => 'required|min:5',
+        'email' => 'required|email',
+        'password' => 'required|min:5',
+        'pasword_confirm' => 'same:password'
+    ];
+
+    public static $updaterules = [
+        'username' => 'required|min: 5',
+        'name' => 'required|min:5',
+        'email' => 'required|email'
+    ];
+}
